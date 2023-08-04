8ee1cf91ce48de79073ca8c1a8a62b13d34a0439
JoannaCz <joannaczeczek3@gmail.com>
2023-08-04 22:13:58 +0200

    Get all data from videos resource

--- Python/YouTubeScript.py
+++ Python/YouTubeScript.py
@@ -5,7 +5,9 @@ page_token = "&"
 country_code = "PL"
 api_key = "AIzaSyDjN-VCXHbQdsCBevxYA9-Jb-wG4dzSh-U"
 
-request_url = f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet&chart=mostPopular&regionCode={country_code}&maxResults=50&key={api_key}"
+request_url = (f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet,contentDetails,"
+               f"liveStreamingDetails,localizations,player,recordingDetails,status,"
+               f"topicDetails&chart=mostPopular&regionCode={country_code}&maxResults=1&key={api_key}")
 request = requests.get(request_url)
 if request.status_code == 429:
     print("Temp-Banned due to excess requests, please wait and continue later")

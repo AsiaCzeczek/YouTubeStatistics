4e0511c09f5c83d180e5f613833afa9135fa44d0
JoannaCz <joannaczeczek3@gmail.com>
2023-08-05 13:35:09 +0200

    Connection to AzureSQL

---
+++ Python/AzureSQL.py
@@ -0,0 +1,19 @@
+import pyodbc
+from dotenv import dotenv_values
+
+secrets = dotenv_values(".env")
+
+server = secrets["AZURE_SQL_SERVER"]
+username = secrets["AZURE_SQL_USERNAME"]
+password = '{' + secrets["AZURE_SQL_PASSWORD"] + '}'
+
+database = 'YoutubeStats'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM dbo.Videos")
+        row = cursor.fetchone()
+        while row:
+            print (row)
+            row = cursor.fetchone()
--- Python/YouTubeScript.py
+++ Python/YouTubeScript.py
@@ -1,16 +1,19 @@
 import requests
 import sys
+from dotenv import dotenv_values
+
+secrets = dotenv_values(".env")
 
 page_token = "&"
 country_code = "PL"
-api_key = "AIzaSyDjN-VCXHbQdsCBevxYA9-Jb-wG4dzSh-U"
+api_key = secrets["YOUTUBE_DATA_API_KEY"]
 
 request_url = (f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet,contentDetails,"
                f"liveStreamingDetails,localizations,player,recordingDetails,status,"
                f"topicDetails&chart=mostPopular&regionCode={country_code}&maxResults=1&key={api_key}")
-request = requests.get(request_url)
-if request.status_code == 429:
+httpResponse = requests.get(request_url)
+if httpResponse.status_code == 429:
     print("Temp-Banned due to excess requests, please wait and continue later")
     sys.exit()
-response = request.json()
+response = httpResponse.json()
 print(response)

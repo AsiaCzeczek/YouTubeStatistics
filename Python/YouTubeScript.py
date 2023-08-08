18b6d0051726b5bef800b8fd1680ced04ad65c46
JoannaCz <joannaczeczek3@gmail.com>
2023-08-08 20:23:31 +0200

    small refactor

--- Python/YouTubeScript.py
+++ Python/YouTubeScript.py
@@ -4,9 +4,6 @@ from dotenv import dotenv_values
 
 secrets = dotenv_values(".env")
 
-country_code = "PL"
-api_key = secrets["YOUTUBE_DATA_API_KEY"]
-
 
 def get_name_and_likes_of_most_popular_video(country_code, api_key):
     request_url = (f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet,contentDetails,"
@@ -21,21 +18,30 @@ def get_name_and_likes_of_most_popular_video(country_code, api_key):
     return name, likes
 
 
-def save_data_to_database(server, database, username, password, name, likes):
-    with pyodbc.connect(
-            'DRIVER= {ODBC Driver 17 for SQL Server};SERVER=tcp:' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
-        with conn.cursor() as cursor:
+def get_db_connection(server, database, username, password):
+    return pyodbc.connect(
+            'DRIVER= {ODBC Driver 17 for SQL Server};SERVER=tcp:' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+
+
+def save_data_to_db(conn, name, likes):
+    with conn as conn2:
+        with conn2.cursor() as cursor:
             sql = "insert into Videos (Name, Likes) values (?, ?)"
             cursor.execute(sql, name, likes)
 
-        conn.commit()
+        conn2.commit()
+        
 
+country_code = "PL"
+api_key = secrets["YOUTUBE_DATA_API_KEY"]
 
 name, likes = get_name_and_likes_of_most_popular_video(country_code, api_key)
+
 
 server = secrets["AZURE_SQL_SERVER"]
 username = secrets["AZURE_SQL_USERNAME"]
 password = '{' + secrets["AZURE_SQL_PASSWORD"] + '}'
 database = 'YoutubeStats'
 
-save_data_to_database(server, database, username, password, name, likes)
+conn = get_db_connection(server, database, username, password)
+save_data_to_db(conn, name, likes)

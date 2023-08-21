0e3d28e101664f2d88f643b0aafa2eeb4e6c2f97
AsiaCzeczek <kowalczykjoanna201@gmail.com>
2023-08-21 13:34:15 +0200

    envs test 3

--- azurefun-youtubedataupdate/__init__.py
+++ azurefun-youtubedataupdate/__init__.py
@@ -2,7 +2,7 @@ import logging
 import azure.functions as func
 import os
 
-env_var = os.environ
+my_secret = os.environ["AZURE_SQL_SERVER"]
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -20,6 +20,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This is kitek!" + str(dict(env_var)),
+             "This is kitek!" + my_secret,
              status_code=200
         )

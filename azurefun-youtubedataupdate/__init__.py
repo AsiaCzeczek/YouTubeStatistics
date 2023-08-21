c53d8e342fea05ebb12d53ffd41ad5e6702441fa
AsiaCzeczek <kowalczykjoanna201@gmail.com>
2023-08-21 11:39:26 +0200

    github env secrets test

--- azurefun-youtubedataupdate/__init__.py
+++ azurefun-youtubedataupdate/__init__.py
@@ -1,7 +1,9 @@
 import logging
-
 import azure.functions as func
+from dotenv import dotenv_values
 
+secrets = dotenv_values(".env")
+secret_val = secrets["SECRET_1"]
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -19,6 +21,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This is kitek!. Pass a name in the query string or in the request body for a personalized response.",
+             "This is kitek!" + secret_val,
              status_code=200
         )

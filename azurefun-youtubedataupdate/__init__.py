a04a461b9e7c7030142102811cffd614badcb228
AsiaCzeczek <kowalczykjoanna201@gmail.com>
2023-08-21 15:13:51 +0200

    moved functions to same file

--- azurefun-youtubedataupdate/__init__.py
+++ azurefun-youtubedataupdate/__init__.py
@@ -1,7 +1,9 @@
 import logging
 import azure.functions as func
 import os
-from . import youtube_script
+import requests
+import pyodbc
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function update DB request.')
@@ -11,9 +13,42 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     username = os.environ["AZURE_SQL_USERNAME"]
     password = '{' + os.environ["AZURE_SQL_PASSWORD"] + '}'
 
-    youtube_script.update_db(api_key, server, username, password)
+    update_db(api_key, server, username, password)
 
     return func.HttpResponse(
              "DB updated",
              status_code=200
         )
+
+def get_name_and_likes_of_most_popular_video(api_key, country_code):
+    request_url = (f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet,contentDetails,"
+                   f"liveStreamingDetails,localizations,player,recordingDetails,status,"
+                   f"topicDetails&chart=mostPopular&regionCode={country_code}&maxResults=1&key={api_key}")
+    http_response = requests.get(request_url)
+    if http_response.status_code == 429:
+        raise Exception("Temp-Banned due to excess requests, please wait and continue later")
+    response = http_response.json()
+    name = response["items"][0]["snippet"]["title"]
+    likes = response["items"][0]["statistics"]["likeCount"]
+    return name, likes
+
+
+def get_db_connection(server, database, username, password):
+    return pyodbc.connect(
+            'DRIVER= {ODBC Driver 17 for SQL Server};SERVER=tcp:' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+
+
+def save_data_to_db(conn, name, likes):
+    with conn as conn2:
+        with conn2.cursor() as cursor:
+            sql = "insert into Videos (Name, Likes) values (?, ?)"
+            cursor.execute(sql, name, likes)
+
+        conn2.commit()
+
+def update_db(api_key, server, username, password):
+    database = 'YoutubeStats'
+    country_code = "PL"
+    name, likes = get_name_and_likes_of_most_popular_video(api_key, country_code)
+    conn = get_db_connection(server, database, username, password)
+    save_data_to_db(conn, name, likes)

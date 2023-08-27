039880fe588c812c0e52ba5c50503b8076ca4c41
AsiaCzeczek <kowalczykjoanna201@gmail.com>
2023-08-27 11:40:42 +0200

    small refactor

---
+++ azurefun-youtubedataupdate/youtube_requests.py
@@ -0,0 +1,11 @@
+def videos_request(videos_count):
+    return (f"videos?part=id,statistics,snippet,contentDetails,"
+            f"liveStreamingDetails,localizations,player,recordingDetails,status,"
+            f"topicDetails&chart=mostPopular&maxResults={videos_count}") 
+
+def videos_dictionaries(response):
+    return [{ 
+            'TableName': 'Videos',
+            'Name': response["items"][0]["snippet"]["title"],
+            'Likes': response["items"][0]["statistics"]["likeCount"]
+        }]

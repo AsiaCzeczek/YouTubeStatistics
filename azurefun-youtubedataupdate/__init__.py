07454630fd86a4dd22b1efbee50ef1851da3a7d2
AsiaCzeczek <141422481+AsiaCzeczek@users.noreply.github.com>
2023-09-09 14:58:01 +0200

    Update __init__.py

--- azurefun-youtubedataupdate/__init__.py
+++ azurefun-youtubedataupdate/__init__.py
@@ -5,7 +5,10 @@ from . import youtube_script
 
 
 def main(myTimer: func.TimerRequest) -> None:
-    logging.info(f'Start db update with timer {"delayed" if myTimer.past_due else "on time"}.')
+    if myTimer.past_due:
+        logging.warning('Start db update with timer delayed.')
+    else:
+        logging.info('Start db update with timer on time.')
 
     api_key = os.environ["YOUTUBE_DATA_API_KEY"]
     server = os.environ["AZURE_SQL_SERVER"]
